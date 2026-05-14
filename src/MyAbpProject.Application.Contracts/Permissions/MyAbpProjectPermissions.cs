cb6bb3c18ddec166535e9d6f0d5cc8f45bc33185
Nick <nick170104@gmail.com>
2026-05-14 21:48:16 +0700

    feat: Implement Author Application Layer

--- src/MyAbpProject.Application.Contracts/Permissions/MyAbpProjectPermissions.cs
+++ src/MyAbpProject.Application.Contracts/Permissions/MyAbpProjectPermissions.cs
@@ -15,4 +15,11 @@ public static class MyAbpProjectPermissions
     }
     //Add your own permission names. Example:
     //public const string MyPermission1 = GroupName + ".MyPermission1";
+    public static class Authors
+    {
+        public const string Default = GroupName + ".Authors";
+        public const string Create = Default + ".Create";
+        public const string Edit = Default + ".Edit";
+        public const string Delete = Default + ".Delete";
+    }
 }
